640a5c4832ade2cb623ee446d81f5a56e8a5a835
sydney <sydneychase5841@gmail.com>
2021-04-11 23:03:19 -0400

    Resubmitting Assignment test are no longer working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick')
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(res=> res.json())
+      .then(results => {
+        results.message.forEach(image => addImage(image))
+      });
+  }
+  function addImage(dogImgUrl) {
+    const container = document.querySelector('#dog-image-container');
+     newImageEl = document.createElement('img');
+    newImageEl.src = dogImgUrl;
+    container.appendChild(newImageEl);
+  }
+  const breeds = [];
+
+  document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+    
+  });
+  
+ const breedUrl = "https://dog.ceo/api/breeds/list/all"
+ fetch(breedUrl)
+ .then(res=> res.json())
+ .then(results => {
+     results.message.forEach()
+ })
